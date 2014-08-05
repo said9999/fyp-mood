1ea324fc0bd10e78069de28d8efe2c1ec05c9a45
Sai <jiangyaoxuan@gmail.com>
2014-08-05 16:02:43 +0800

    enalbe font-end storage

--- app/assets/javascripts/controllers/PanasTest.js
+++ app/assets/javascripts/controllers/PanasTest.js
@@ -3,19 +3,32 @@ Mood.PanasTestController = Ember.ObjectController.extend({
   startIndex : 1,
   imgRootUrl : "assets/panas_photos/",
   imgUrl : "assets/panas_photos/1.jpg", 
+  scores : [1,5,5,5,5,5,5,5,5,5],
 
   actions: {
   	nextTest : function(){
-  		
-  		alert('in nexttest');
+  		//alert('in nexttest');
+      var score = $('#sl1').slider('getValue');
+      this.scores[this.startIndex-1] = score;
+
   		this.set('startIndex',this.get('startIndex')+1);
+      $('#sl1').slider('setValue',this.scores[this.startIndex-1]);
+
   		this.set('imgUrl',this.get('imgRootUrl')+this.get('startIndex')+".jpg");
  
   	},
 
   	previousTest : function(){
-  		alert('in previousTest');
+  		//alert('in previousTest');
+
+      var score = $('#sl1').slider('getValue');
+      this.scores[this.startIndex-1] = score;
+
+      alert(this.scores);
+
   		this.set('startIndex',this.get('startIndex')-1);
+      $('#sl1').slider('setValue',this.scores[this.startIndex-1]);
+
   		this.set('imgUrl',this.get('imgRootUrl')+this.get('startIndex')+".jpg");
   	}
   }
