e406df63d17874c568a9c4a6cb36a34a18985369
Sai <jiangyaoxuan@gmail.com>
2014-08-11 01:17:53 +0800

    pam finsih

--- app/assets/javascripts/views/pamgalleryviews/pamgalleryview.js
+++ app/assets/javascripts/views/pamgalleryviews/pamgalleryview.js
@@ -1,24 +1,67 @@
 Mood.PamGalleryView = Ember.View.extend({
 	templateName : 'pamgalleryviews/pamgalleryviews',
 	didInsertElement: function(){
-		$('.galleryPic').css('z-index',0);
+		/*******************
+			funcitons
+		 *******************/
+		Array.prototype.contains = function ( needle ) {
+   			for (i in this) {
+       			if (this[i] == needle) return true;
+   			}
+
+   			return false;
+		};
+
+		 function randomPhotos(){
+		 	var mySet = [];
+		 	var max=21;
+
+		 	while(mySet.length < 9){
+		 		var id = Math.random() * 10000 % max;
+		 		id = Math.floor(id);
+
+		 		if(!mySet.contains(id))
+		 			mySet.push(id);
+		 	}
+		 	return mySet;
+		 };
+
+		 function replacePhotos(array){
+		 	for(i=0;i<9;i++){
+				var id = '#pic'+i;
+				$(id).attr("src","assets/PAM/" + array[i] + ".jpg"); 
+			}
+		 };
+
+		/********************
+			insert photos
+		*********************/
+		var array = randomPhotos();
+	 	replacePhotos(array);
+
+		/*******************
+			Brightness
+		********************/
+		$('.galleryPic').css('-webkit-filter','brightness(0.7)');
+		$('.galleryPic').css('filter', 'brightness(0.7)');			
+
+
 		$('.galleryPic').hover(function(e){
-			$(this).animate({
-				'width':'320px',
-				'height':'auto',
-				 'z-index':'1'
-
-			},300);
-			
-
+			$(this).css('-webkit-filter','brightness(1.0)');
+			$(this).css('filter', 'brightness(1.0)');		
 		});
 
 		$('.galleryPic').mouseout(function(e){
-			$(this).animate({
-				'width':'280px',
-				'height':'auto',
-				 'z-index':'0'
-			},200);
+			$(this).css('-webkit-filter','brightness(0.7)');
+			$(this).css('filter', 'brightness(0.7)');		
 		});
+
+		/********************
+		 	try other photos
+		 ********************/
+	 	$('.newset').click(function(){
+	 		var array = randomPhotos();
+	 		replacePhotos(array);
+	 	});
 	}
 });
