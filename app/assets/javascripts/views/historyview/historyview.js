c9692ea9fb23ac5e73c072ecbe60d40bae1e63c7
Sai <jiangyaoxuan@gmail.com>
2014-08-20 15:50:25 +0800

    add google chart

--- app/assets/javascripts/views/historyview/historyview.js
+++ app/assets/javascripts/views/historyview/historyview.js
@@ -1,7 +1,25 @@
 Mood.HistoryView = Ember.View.extend({
 	templateName : 'historyview/history',
 
-	didInsertElement: function(){
+	didInsertElement: function(){ 
+	  function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Year', 'Sales', 'Expenses'],
+          ['2004',  1000,      400],
+          ['2005',  1170,      460],
+          ['2006',  660,       1120],
+          ['2007',  1030,      540]
+        ]);
+
+        var options = {
+          title: 'Company Performance'
+        };
+
+        var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
+      
+      google.setOnLoadCallback(drawChart);
 
 	}
 });
