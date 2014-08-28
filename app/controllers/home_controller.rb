8dc084b34a087971e63a448112b325866cc15146
Sai <jiangyaoxuan@gmail.com>
2014-08-28 15:30:01 +0800

    add login page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
   def index
+  	
+  end
+
+  def login
+
+  end
+
+  def create
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Mood::Application.routes.draw do
   post '/data_access/:type', to:'data#read' 
   post '/data_update/:type', to: 'data#update'
 
+  get '/login', to: 'home#login'
   get '*path',to: 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
